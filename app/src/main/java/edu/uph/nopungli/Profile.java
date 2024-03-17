6d89db5f0f92b03b6e1ac4a006f4e7a4e1988023
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2024-03-18 01:45:00 +0700

    membuat intent
    pindah dari profile activity ke main activity

--- app/src/main/java/edu/uph/nopungli/Profile.java
+++ app/src/main/java/edu/uph/nopungli/Profile.java
@@ -2,13 +2,25 @@ package edu.uph.nopungli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Profile extends AppCompatActivity {
-
+    ImageButton backLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        backLogin.findViewById(R.id.backLogin);
+        backLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Profile.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
