867bd1ce7203706a74781b7d5915f72a24eee9f5
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2024-04-06 20:30:43 +0700

    mengedit login page
    buat radius dan ubah ccolor pallete

--- app/src/main/java/edu/uph/nopungli/MainActivity.java
+++ app/src/main/java/edu/uph/nopungli/MainActivity.java
@@ -2,13 +2,24 @@ package edu.uph.nopungli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signin = (TextView) findViewById(R.id.signin);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
