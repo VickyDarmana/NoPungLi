3648cb65dced35b2b7bfb6dbce5af70766026090
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2024-04-06 20:46:01 +0700

    refactor nama file
    ubah nama satu activity

--- app/src/main/java/edu/uph/nopungli/MainActivity.java
+++ app/src/main/java/edu/uph/nopungli/MainActivity.java
@@ -2,7 +2,6 @@ package edu.uph.nopungli;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -12,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         signin = (TextView) findViewById(R.id.signin);
         signin.setOnClickListener(new View.OnClickListener() {
