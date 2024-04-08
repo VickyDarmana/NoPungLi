4d46d267f9e76b6ad1eaa3a45dad789fb308520f
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2024-04-09 00:45:48 +0700

    asdasdw
    asqaw

--- app/src/main/java/edu/uph/nopungli/Login.java
+++ app/src/main/java/edu/uph/nopungli/Login.java
@@ -5,20 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-    TextView signin;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        signin = findViewById(R.id.signin);
-        signin.setOnClickListener(new View.OnClickListener() {
+        btnLogin.findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, HomeFragment.class);
+                Intent intent = new Intent(Login.this, Home.class);
                 startActivity(intent);
             }
         });
