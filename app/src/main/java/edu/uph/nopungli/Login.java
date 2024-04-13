0e7410e2817d76d7c5cc17f42c77810d30b4a8bb
VerrelAngkasa <134504630+VerrelAngkasa@users.noreply.github.com>
2024-04-14 00:36:35 +0700

    melakukan fragment transaction
    dari activity login menuju fragment home

--- app/src/main/java/edu/uph/nopungli/Login.java
+++ app/src/main/java/edu/uph/nopungli/Login.java
@@ -1,6 +1,8 @@
 package edu.uph.nopungli;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,9 +21,10 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login.this, Home.class);
-                startActivity(intent);
-            }
+                Fragment fragment = new HomeFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.container,fragment).commit();
+            };
         });
     }
 }
